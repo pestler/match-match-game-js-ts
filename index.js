39d2aaad947791174f6e1e73ffa7a8abdb918f6a
pestler <oleh@tut.by>
2021-06-25 12:57:13 +0300

    1 file modified

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Started</h1>`;
 
+const deckCard = ['Agility.png','Agility.png','Boat.png','Boat.png',
+'Citizenship.png','Citizenship.png','Hack.png','Hack.png',
+]
+
 
 
