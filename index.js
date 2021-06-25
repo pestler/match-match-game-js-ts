4e390c57f3d87868995c8160690d137a3e5035d6
pestler <oleh@tut.by>
2021-06-25 13:38:41 +0300

    1 file modified

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ appDiv.innerHTML = `<h1>JS Started</h1>`;
 
 const deckCard = ['Agility.png','Agility.png','Boat.png','Boat.png',
 'Citizenship.png','Citizenship.png','Hack.png','Hack.png',
+'Nerd-Rage.png','Nerd-Rage.png','Nuka-Cola.png','Nuka-Cola.png',
+'Robotics.png','Robotics.png','Shock.png','Shock.png'
 ]
 
 
