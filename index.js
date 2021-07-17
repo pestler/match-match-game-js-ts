bdfa505ad56381e2d852197e5ea7820f604a83e1
pestler <oleh@tut.by>
2021-07-17 10:55:01 +0300

    1 file modified

--- index.js
+++ index.js
@@ -200,4 +200,30 @@ function winGame (){
   }
 }
 
+deck.addEventListener("click",function(evt){
+if(evt.target.nodeName ==="LI"){
+  console.log(evt.target.nodeName + "Was clicked");
+  if(timeStart === false){
+    timeStart === true;
+    timer();
+  }
+  flipCard();
+}
+function flipCard(){
+  evt.target.classList.add("flip");
+  addToOpened();
+}
+function addToOpened(){
+  if (opened.length ===0 || opened.length ===1){
+    opened.push(evt.target.firstElementChild)
+  }
+  compareTwo();
+}
+});
+
+reset.addEventListener('click', resetEverything);
+playAgain.addEventListener('click',function(){
+  modal.style.display = "none";
+  resetEverything();
+})
 
