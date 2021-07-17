c73268956c47b923f84ede91c0d237213b92bc85
pestler <oleh@tut.by>
2021-07-17 09:59:06 +0300

    1 file modified

--- index.js
+++ index.js
@@ -40,7 +40,9 @@ let minutes = 0;
 let seconds = 0;
 let timeStart = false;
 function shuffle(array) {
-  let currentIndex = array.length,    temporaryValue,    randomIndex;
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -67,37 +69,42 @@ function startGame() {
 
 startGame();
 
-function removeCard(){
-  while(deck.hasChildNodes()){
-deck.removeChild(deck.firstChild)
+function removeCard() {
+  while (deck.hasChildNodes()) {
+    deck.removeChild(deck.firstChild);
   }
 }
 
-function timer(){
-  time = setInterval(function(){
+function timer() {
+  time = setInterval(function() {
     seconds++;
-    if(seconds  === 60){
+    if (seconds === 60) {
       minutes++;
       seconds = 0;
     }
-    timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>"+"Timer:" + minutes + "Mins" + seconds + "Secs";
-  }, 1000)
+    timeCounter.innerHTML =
+      "<i class='fa fa-hourglass-start'></i>" +
+      'Timer:' +
+      minutes +
+      'Mins' +
+      seconds +
+      'Secs';
+  }, 1000);
 }
 
-
-function stopTime(){
-  clearInterval(time)
+function stopTime() {
+  clearInterval(time);
 }
 
-function resetEverything(){
+function resetEverything() {
   stopTime();
   timeStart = false;
   seconds = 0;
   minutes = 0;
-  timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + 
-  "Timer: 00:00";
-  star[1].firstElementChild.classList.add("fa-star");
-  star[2].firstElementChild.classList.add("fa-star");
+  timeCounter.innerHTML =
+    "<i class='fa fa-hourglass-start'></i>" + 'Timer: 00:00';
+  star[1].firstElementChild.classList.add('fa-star');
+  star[2].firstElementChild.classList.add('fa-star');
   starCount = 3;
   moves = 0;
   movesCount.innerHTML = 0;
@@ -107,68 +114,90 @@ function resetEverything(){
   startGame();
 }
 
-function movesCounter(){
-  movesCount.innerHTML ++;
-  moves ++;
+function movesCounter() {
+  movesCount.innerHTML++;
+  moves++;
 }
 
-function starRating(){
-  if (moves === 14){
-    star[2].firstElementChild.classList.remove("fa-star");
+function starRating() {
+  if (moves === 14) {
+    star[2].firstElementChild.classList.remove('fa-star');
     starCount--;
   }
-  if (moves === 18){
-    star[1].firstElementChild.classList.remove("fa-star");
+  if (moves === 18) {
+    star[1].firstElementChild.classList.remove('fa-star');
     starCount--;
   }
 }
 
-function compareTwo(){
-  if (opened.length ===2){
+function compareTwo() {
+  if (opened.length === 2) {
     document.body.stylepointerEvents = 'none';
   }
-  if(opened.length ===2 && opened[0].src === opened[1].src){
-match();
-  } else if (opened.length === 2 && opened[0].src != opened[1].src){
-    noMatch()
+  if (opened.length === 2 && opened[0].src === opened[1].src) {
+    match();
+  } else if (opened.length === 2 && opened[0].src != opened[1].src) {
+    noMatch();
   }
 }
 
-function match(){
-  setTimeout(function(){
-    opened[0].parentElement.classList.add("match");
-    opened[1].parentElement.classList.add("match");
+function match() {
+  setTimeout(function() {
+    opened[0].parentElement.classList.add('match');
+    opened[1].parentElement.classList.add('match');
     matched.push(...opened);
-    document.body.style.pointerEvents = "auto";
+    document.body.style.pointerEvents = 'auto';
     winGame();
     opened = [];
-  },600);
+  }, 600);
   movesCounter();
   starRating();
 }
 
-function noMatch(){
-  setTimeout(function(){
-    opened[0].parentElement.classList.remove("flip");
-    opened[1].parentElement.classList.remove("flip");
-    document.body.style.pointerEvents = "auto";
+function noMatch() {
+  setTimeout(function() {
+    opened[0].parentElement.classList.remove('flip');
+    opened[1].parentElement.classList.remove('flip');
+    document.body.style.pointerEvents = 'auto';
     opened = [];
-  },700)
+  }, 700);
   movesCounter();
   starRating();
-  }
+}
 
-  function AddStats(){
-    const stats = document.querySelector("modal-content");
-    for(let i = 1; i <=3; i++){
-      const statsElement = document.createElement("p");
-      statsElement.classList.add("stats");
-      stats.appendChild(statsElement);
+function AddStats() {
+  const stats = document.querySelector('modal-content');
+  for (let i = 1; i <= 3; i++) {
+    const statsElement = document.createElement('p');
+    statsElement.classList.add('stats');
+    stats.appendChild(statsElement);
+  }
+  let p = stats.querySelectorAll('p.stats');
+  p[0].innerHTML =
+    'Time to complete:' + minutes + 'Minutes and' + seconds + 'Seconds';
+  p[1].innerHTML = 'Moves Taken:' + moves;
+  p[2].innerHTML = 'Your Star Rating is:' + starCount + 'out of 3';
+}
+
+function displayModal() {
+  const modalClose = document.getElementsByClassName('close')[0];
+  modal.style.display = 'block';
+  modalClose.onclick = function() {
+    modal.style.display = 'none';
+  };
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
     }
-    let p = stats.querySelectorAll("p.stats");
-    p[0].innerHTML = "Time to complete:" + minutes + "Minutes and" + seconds + "Seconds";
-    p[1].innerHTML = "Moves Taken:" + moves;
-    p[2].innerHTML = "Your Star Rating is:" + starCount + "out of 3";
-  }
+  };
+}
 
-  
+function winGame (){
+  if(matched.length ==16){
+    stopTime();
+    AddStats();
+    displayModal();
+  }
+}
+
+
